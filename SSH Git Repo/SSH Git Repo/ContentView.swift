3c3b25d8cd638ebd28e3a382bc6f64fdf1d56939
Voffkal <voffkal@hotmail.com>
2023-08-27 19:07:23 +0900

    Second commit

---
+++ SSH Git Repo/SSH Git Repo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SSH Git Repo
+//
+//  Created by Vladimir on 8/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SSH Git Repo/SSH Git Repo/SSH_Git_RepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSH_Git_RepoApp.swift
+//  SSH Git Repo
+//
+//  Created by Vladimir on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SSH_Git_RepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
